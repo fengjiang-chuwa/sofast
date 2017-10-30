030ddb4ce8e8f43a6ae07c5bce562aa2bd6093c6
vivianyyf <vivianyyf@foxmail.com>
2017-10-30 16:45:05 +0800

    add send email button

--- html/js/datatables.js
+++ html/js/datatables.js
@@ -4,13 +4,14 @@ validationApp.controller('add_courses', add_courses);
 function add_courses($scope, $compile, DTOptionsBuilder, DTColumnBuilder) {
   var self = this;
   //self.dtOptions = DTOptionsBuilder.fromSource('https://l-lin.github.io/angular-datatables/data.json')
-  self.dtOptions = DTOptionsBuilder.fromSource('/sofast/html/js/json/student.json').withPaginationType('full_numbers').withOption('createdRow', createdRow);
+  self.dtOptions = DTOptionsBuilder.fromSource('/sofast/html/json/student.json').withPaginationType('full_numbers').withOption('createdRow', createdRow);
   self.dtColumns = [
     // DTColumnBuilder.newColumn('').withTitle('#').renderWith(function(data, type, row, meta) {
     //   return (meta.row + 1);
     // }),
     DTColumnBuilder.newColumn('name').withTitle('Student Name'),
     DTColumnBuilder.newColumn('email').withTitle('Email'),
+    DTColumnBuilder.newColumn('student_status').withTitle('Status'),
     DTColumnBuilder.newColumn('').withTitle('Actions').renderWith(actionHtml)
   ];
   $scope.searchApplicant = function() {
@@ -23,7 +24,12 @@ function add_courses($scope, $compile, DTOptionsBuilder, DTColumnBuilder) {
   }
 
   function actionHtml(data, type, row) {
-    return ' \
-    <a href = "student_details.html" class = "btn-default btn btn-xs"> <span class = "glyphicon glyphicon-search"> </span> View Details</a>';
+    var btn;
+    if (row.student_status == "replied") {
+      btn = '<a href = "student_details.html" class = "btn-default btn btn-xs"> <span class = "glyphicon glyphicon-search"> </span> View Details</a>'
+    } else if (row.student_status == "New") {
+      btn = '<a href = "student_details.html" class = "btn-default btn btn-xs"> <span class = "glyphicon glyphicon-envelope"> </span> Send Email</a>'
+    } else btn = '';
+    return btn;
   }
 }
