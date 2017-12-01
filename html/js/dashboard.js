cf4be7db8e408e92754cefca8e70007d3d326af3
vivianyyf <vivianyyf@foxmail.com>
2017-12-01 13:25:57 +0800

    Dashboard Template for Bootstrap

---
+++ html/js/dashboard.js
@@ -0,0 +1,12 @@
+document.write("<link rel = 'stylesheet' type = 'text/css' href = '/sofast/html/plugin/jquery/css/jquery.dataTables.css'>");
+document.write("<link rel = 'stylesheet' type = 'text/css' href = '/sofast/html/plugin/bootstrap/css/bootstrap.css'>");
+document.write("<link rel = 'stylesheet' type = 'text/css' href = '/sofast/html/css/dashboard.css'>");
+document.write("<script  type='text/javascript' src='/sofast/html/plugin/jquery/js/jquery-1.11.3.min.js'></script>");
+document.write("<script  type='text/javascript' src='/sofast/html/plugin/jquery/js/jquery.dataTables.min.js'></script>");
+document.write("<script  type='text/javascript' src='/sofast/html/plugin/angular/js/angular.js'></script>");
+document.write("<script  type='text/javascript' src='/sofast/html/plugin/angular/js/angular-datatables.min.js'></script>");
+document.write("<script  type='text/javascript' src='/sofast/html/plugin/bootstrap/js/ui-bootstrap-tpls-1.3.3.js'></script>");
+document.write("<script  type='text/javascript' src='/sofast/html/js/ui.js'></script>");
+document.write("<script  type='text/javascript' src='/sofast/html/js/datatables.js'></script>");
+document.write("<script  type='text/javascript' src='/sofast/html/plugin/bootstrap/js/popper.min.js'></script>");
+document.write("<script  type='text/javascript' src='/sofast/html/plugin/bootstrap/js/bootstrap.min.js'></script>");
