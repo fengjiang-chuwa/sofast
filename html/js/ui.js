fb0f2711498e3edb8f322bbbe76af68ef033e7cf
vivianyyf <vivianyyf@foxmail.com>
2017-12-28 10:16:11 +0800

    add angular query

--- html/js/ui.js
+++ html/js/ui.js
@@ -2,8 +2,7 @@
 function goBack() {
     window.history.go(-1)
 }
-
-var validationApp = angular.module('soFast', ['ui.bootstrap', 'datatables']);
+var validationApp = angular.module('soFast', ['ui.bootstrap', 'datatables', 'angular-query-builder']);
 // show Loading 
 validationApp.controller('ShowLoadingCtrl', function($scope) {
     var self = this;
@@ -91,7 +90,6 @@ validationApp.controller('DatepickerPopupDemoCtrl', function($scope) {
         return '';
     }
 });
-
 // Create Phone Number Controller
 validationApp.controller('phoneNumber', ['$scope', function($scope) {
     $scope.data = {
@@ -99,30 +97,11 @@ validationApp.controller('phoneNumber', ['$scope', function($scope) {
         availableOptions: [{ id: '+86', name: 'China (+86)' }, { id: '2', name: 'China (+86)China (+86)China (+86)China (+86)' }, { id: '3', name: 'Option C' }]
     };
 }]);
-
 // Show Information
 validationApp.controller('showInfo', function($scope, $window) {
     $scope.level = ['Undergraduate', 'Postgraduate', 'Postgraduated Researcher'];
     $scope.files = ['Passport', 'Academic Transcript', 'English Language Report / Certificate', 'Existing I-20', 'Personal Statement / Admission Essay', 'CV', 'Certificate (Optional)']
-    $scope.bragSheet = [
-        '1. List 1-3 occupations you are considering in order of preference and explain what it is about the occupation that interests you and why you might be successful in such a field.',
-        '2. Describe how a particular event or activity (positive or negative) has had the most significant impact on you in your life and shaped you as a person and why?',
-        "3. Please describe your family in detail (ex: siblings and their age, which do you, live with, mother/father's occupation and education etc.).",
-        "4. Pick five original adjectives that you believe best describes who you. For each, write a sentence as to why you chose it.",
-        "5. Discuss any significant decrease or increase in your academic performance and why?",
-        "6.  Compare yourself from 9th grade to now.",
-        "7. What hobby, talents or endeavor are you passionate about and why? How have you pursued this passion?",
-        "8. Do you work or are completing an internship? If so, where do you work or serve as an intern? What have you learned on the job or through your internship that school couldn't have taught? What do you do with your work income?",
-        "9. Discuss one act of leadership you have demonstrated in your life.",
-        "10. What is your favorite quote and why?",
-        "11. Describe a class or extracurricular activity that you are most proud of, and how it illustrates your best qualities.",
-        "12. School Related Activities: Please describe and indicate the dates of affiliations (clubs, teams, service organizations, student government, etc.). If you hold/held an office or had a specific responsibility, please describe each activity with the most recent first.",
-        "13. Non School Related Activities: That you have participated in (work experience, internships non-school sponsored organizations) where, what and when you started and how long (be specific):",
-        "14. Community Service: Where, what and when you started and how long (be specific):",
-        "15. Awards and honors: That you have earned; where, what and why (be specific):",
-        "16. Anything you think is of importance to your application to colleges, that without this you application will be incomplete"
-
-    ];
+    $scope.bragSheet = ['1. List 1-3 occupations you are considering in order of preference and explain what it is about the occupation that interests you and why you might be successful in such a field.', '2. Describe how a particular event or activity (positive or negative) has had the most significant impact on you in your life and shaped you as a person and why?', "3. Please describe your family in detail (ex: siblings and their age, which do you, live with, mother/father's occupation and education etc.).", "4. Pick five original adjectives that you believe best describes who you. For each, write a sentence as to why you chose it.", "5. Discuss any significant decrease or increase in your academic performance and why?", "6.  Compare yourself from 9th grade to now.", "7. What hobby, talents or endeavor are you passionate about and why? How have you pursued this passion?", "8. Do you work or are completing an internship? If so, where do you work or serve as an intern? What have you learned on the job or through your internship that school couldn't have taught? What do you do with your work income?", "9. Discuss one act of leadership you have demonstrated in your life.", "10. What is your favorite quote and why?", "11. Describe a class or extracurricular activity that you are most proud of, and how it illustrates your best qualities.", "12. School Related Activities: Please describe and indicate the dates of affiliations (clubs, teams, service organizations, student government, etc.). If you hold/held an office or had a specific responsibility, please describe each activity with the most recent first.", "13. Non School Related Activities: That you have participated in (work experience, internships non-school sponsored organizations) where, what and when you started and how long (be specific):", "14. Community Service: Where, what and when you started and how long (be specific):", "15. Awards and honors: That you have earned; where, what and why (be specific):", "16. Anything you think is of importance to your application to colleges, that without this you application will be incomplete"];
 });
 // function loadMenu(name,name1,search){
 //  $("body").hide();
